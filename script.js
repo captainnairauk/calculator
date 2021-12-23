1ac1e813b4ba9491c670dfe45e82abafc41e07e3
Aniket Kaimal <aniket1995@yahoo.co.in>
2021-12-24 02:02:02 +0530

    updated the para.textContent.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function divide(a, b) {
     return a / b;
 }//divides two numbers
 
-function operator(operator, a, b) {
+function operator(a, b, operator) {
     if (operator == add) {
         return add(a, b);
     } else if (operator == subtract) {
@@ -27,13 +27,21 @@ function operator(operator, a, b) {
 }//calls operators (add, subtract, etc) on two numbers and returns the result
 
 const buttons = document.querySelectorAll(".key");
-
 for (let button of buttons) {
     button.addEventListener("click", function (e) {
         let numbers = e.target.textContent;
-        para.textContent = numbers;
+        para.textContent = para.textContent+numbers;
+        if(para.textContent.length>9){
+            output.addEventListener(function)
+            // let integer = parseInt(para.textContent);
+            // let exponents = integer.toExponential(2);
+            // para.textContent=exponents;
+            // console.log(integer);
+        }
     });
 }//displays the innerText after pressing the buttons (0-9)
+
+
 
 const output = document.querySelector(".output");
 const para = document.createElement("p");
