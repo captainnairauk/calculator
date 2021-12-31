a80aa81e662f36f614bc1ebb884e0b273fcdb45e
Aniket Kaimal <aniket1995@yahoo.co.in>
2022-01-01 01:42:09 +0530

    Made the calculator functional (only partially), more work to do.

--- script.js
+++ script.js
@@ -1,72 +1,71 @@
 function add(a, b) {
-    return parseInt(a) + parseInt(b);
+    return a + b;
 }//adds two numbers
 
 function subtract(a, b) {
-    return parseInt(a) - parseInt(b);
+    return a - b;
 }//subtracts two numbers
 
 function multiply(a, b) {
-    return parseInt(a) * parseInt(b);
+    return a * b;
 }//multiplies two numbers
 
 function divide(a, b) {
-    return parseInt(a) / parseInt(b);
+    return a / b;
 }//divides two numbers
 
-function operator(a, operator, b) {
-    if (operator == add) {
+function operate(a, operator, b) {
+    if (operator === "+") {
         return add(a, b);
-    } else if (operator == subtract) {
+    } else if (operator === "-") {
         return subtract(a, b);
-    } else if (operator == multiply) {
+    } else if (operator === "x") {
         return multiply(a, b);
     } else {
         return divide(a, b);
     }
 }//calls operators (add, subtract, etc) on two numbers and returns the result
 
+// console.log(operate(78,"/",2));
 const lowerDisplay = document.querySelector(".lower-display");
 const upperDisplay = document.querySelector(".upper-display");
 const numButtons = document.querySelectorAll(".key");
 const operators = document.querySelectorAll(".operator");
 const dot = document.querySelector(".dot");
 
-let array1 = [];
-let array2 = [];
-let x = "0";
-let y = "0";
+let array1 = ["0"];
+let array2 = ["0"];
+
+
 
 for (let button of numButtons) {
     button.addEventListener("click", function (e) {
         let numbers = e.target.textContent;
         lowerDisplay.textContent = lowerDisplay.textContent + numbers;
-        array1.push(lowerDisplay.textContent);
-        x = array1[array1.length - 1];
-        console.log(x);
     });
 };//displays the innerText of numericals(0-9)
 
 for (let operator of operators) {
     operator.addEventListener("click", function (e) {
         let operator = e.target.textContent;
+        array2.push(operator);
         upperDisplay.textContent = lowerDisplay.textContent + operator;
-        lowerDisplay.textContent="";   
+        array1.push(lowerDisplay.textContent);
+        lowerDisplay.textContent = "";
+        let a = array1[array1.length - 2];
+        let b = array1[array1.length - 1];
+        let c = array2[array2.length - 2];
+        let d = array2[array2.length - 1];
+        console.table(a);
+        if (operator === "=") {
+            upperDisplay.textContent = a + c + b + d;
+            lowerDisplay.textContent = operate(a, c, b);
+        }
+        // lowerDisplay.textContent="";
     });
 };//displays the innerText of operators(+,-,etc.)
 
-// populateOp();
 
-// let numbers = e.target.textContent;
-// para.textContent = para.textContent+numbers;
-// return para.textContent;
-// if(para.textContent.length>9){
-//     output.addEventListener(function)
-//     let integer = parseInt(para.textContent);
-//     let exponents = integer.toExponential(2);
-//     para.textContent=exponents;
-//     console.log(integer);
-// }
 
 
 
