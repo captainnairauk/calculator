7fa4a3c2fcbab97e6d91cd34d04a5fe8c2456168
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2025-01-11 18:30:59 +0530

    Add variables to store calculator operation components

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+
+firstNumber = 3;
+operator = "+";
+secondNumber = 5;
+
+console.log(`Operation: ${firstNumber} ${operator} ${secondNumber}`);
+
+
 function add(a, b) {
   return a + b;
 }
@@ -17,8 +28,3 @@ function divide(a, b) {
   return a / b;
 }
 
-console.log("Add: ", add(5, 3));
-console.log("Subtract: ", subtract(5, 3));
-console.log("Multiply: ", multiply(5, 3));
-console.log("Divide: ", divide(6, 3));
-console.log("Divide by zero: ", divide(5, 0));
